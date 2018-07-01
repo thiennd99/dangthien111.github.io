546b7cd8c97f41ed0d70b3db4357c0a056627c33
thien <thienlcvip@gmail.com>
2018-07-01 10:51:40 +0700

    update

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,11 +1,53 @@
+let i = 0;
 function getValue(e){
+    if(i == 1){
+        document.getElementById('display').value = '';
+        document.getElementById('display').value += e.value;
+        i =0;
+    }else{
     document.getElementById("display").value += e.value;
-    
+    }
 }
-var theNum = '',
-    oldNum = '',
-    resultNum,
-    operators;
+
 function clearAll() {
     document.getElementById("display").value = '';
 }
+function equal(){
+  var a = document.getElementById("display").value;
+  let b = eval(a);  f
+  i = 1;
+  if(b<1){
+     document.getElementById("display").value = b.toFixed(1);
+     
+  }
+  else   {document.getElementById("display").value = b;
+  
+}
+  
+}
+function square(){
+   var a = document.getElementById("display").value;
+  var b = a*a;
+  document.getElementById("display").value = b;
+}
+function factorialize(){
+  let a = document.getElementById("display").value;
+  console.log(typeof a);
+  let b = parseInt(a); 
+  if ( b == 1 || b == 0){
+        document.getElementById('display').value = "1"
+      }else{
+    for(let i = b - 1; i>=1; i-- ){  
+   b *= i;
+     }
+        document.getElementById('display').value = b;
+    }  
+}
+function squareRoot(){
+  let a = document.getElementById('display').value;
+  let b = parseInt(a);
+  let c = Math.sqrt(b)
+  document.getElementById('display').value = c;
+  
+} 
+
