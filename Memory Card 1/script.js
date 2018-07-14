926709d16f668b7bd79414d32862353bcef4e02e
haidoan <haidoan9xx@gmail.com>
2018-07-13 22:27:30 -0700

    thien cui bap vkl

--- Memory Card 1/script.js
+++ Memory Card 1/script.js
@@ -1,12 +1,40 @@
-function chooseCart(e){
-    console.log(e)
-    console.log(  $(e).find('.front')    );
-    let a = $(e).find('.front');
-    let b = $(e).find('.back');
-    $(a).css('transform','rotateY(0deg)');
-    $(b).css('transform','rotateY(180deg)');
-    $(a).css('backface-visibility','hidden');
-    $(b).css('backface-visibility','hidden');
-    $(a).css('transition','0.4s');
-    $(b).css('transition','0.4s');
-}
+const card = $('.card__inner')
+
+
+card.click(function(e){
+    alert('Bạn đã bị hack !!!')
+    $(this).find('.back').addClass('new_back');
+    $(this).find('.front').addClass('new_front');
+});
+
+// card.forEach(element => {
+//     element.addEventListener('click',function(e){
+//     this.classList.add('new_front');
+   
+//     // a.css('transform','rotateY(0deg)');
+//     // b.css('transform','rotateY(180deg)');
+//     // a.css('backface-visibility','hidden');
+//     // b.css('backface-visibility','hidden');
+//     // a.css('transition','0.4s');
+//     // b.css('transition','0.4s');
+// console.log(element)
+// });
+
+// });
+
+
+
+// let a = $('.card__inner');
+// a.on('click', function(){
+// a.find('.front').css('transform','rotateY(0deg)');
+// a.find('.front').css('backface-visibility','hidden');
+// a.find('.front').css('transition','0.4s');
+// a.find('.back').css('transform','rotateY(180deg)');
+// a.find('.back').css('backface-visibility','hidden');
+// a.find('.back').css('transition','0.4s');
+// })
+
+
+
+
+
